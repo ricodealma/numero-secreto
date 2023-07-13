065218ae8b9f317ed2c353b0723df2c2ca31e6fe
ricodealma <fael9339@gmail.com>
2023-07-12 21:28:49 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -1,7 +1,20 @@
 function verificaSeOChutePossuiUmValorValido(chute){
     const numero = + chute
-    if (Number.isNaN(numero)) {
+    if (chuteForInvalido(numero)) {
         console.log('Valor inválido')
     }
 
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        console.log(`valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`)
+    }
+
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor    
 }
