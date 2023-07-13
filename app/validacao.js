971cc14182d2ae3d6da627762ac9b9a8b3bc821e
ricodealma <fael9339@gmail.com>
2023-07-12 22:06:59 -0300

    Botao RETRY

--- app/validacao.js
+++ app/validacao.js
@@ -14,6 +14,8 @@ function verificaSeOChutePossuiUmValorValido(chute){
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">RETRY</button>
         `
     }else if (chute > numeroSecreto) {
         elementoChute.innerHTML += `<div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`        
@@ -31,3 +33,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor    
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id =='jogar-novamente') {
+        window.location.reload()
+    }
+})
