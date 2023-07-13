fd8246001168b3a69616157b8146b23e83616efa
ricodealma <fael9339@gmail.com>
2023-07-12 21:16:30 -0300

    VerificaChute

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -12,6 +12,7 @@ recognition.addEventListener('result', onSpeak)
 function onSpeak(e){
     chute = e.results[0][0].transcript
     exibeChuteNaTela(chute)
+    
 }
 
 function exibeChuteNaTela(params) {
---
+++ app/validacao.js
@@ -0,0 +1,7 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numer = + chute
+    if (Number.isNaN(numero)) {
+        console.log('Valor inválido')
+    }
+
+}
