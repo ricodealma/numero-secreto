3fb8c835d8b569e4a05e426e3e9c5ad1c775e6ad
ricodealma <fael9339@gmail.com>
2023-07-12 22:30:05 -0300

    tentativa game over

--- app/validacao.js
+++ app/validacao.js
@@ -1,6 +1,14 @@
 function verificaSeOChutePossuiUmValorValido(chute){
     const numero = + chute
     if (chuteForInvalido(numero)) {
+        if (numero == 'Game Over' || 'game over' || 'Game over') {
+            document.body.innerHTML = `
+        <h2>GAME OVER !</h2>
+        <button id="jogar-novamente" class="btn-jogar">retry ?</button>
+        `
+        } else {
+            
+        }
         elementoChute.innerHTML += '<div> Valor inválido </div>'
         return
     }
@@ -15,7 +23,7 @@ function verificaSeOChutePossuiUmValorValido(chute){
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
 
-        <button id="jogar-novamente" class="btn-jogar">RETRY</button>
+        <button id="jogar-novamente" class="btn-jogar">retry</button>
         `
     }else if (chute > numeroSecreto) {
         elementoChute.innerHTML += `<div> O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`        
